59a3660fc37870f7d0b91265abe828a549e6342c
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-28 14:34:03 -0300

    feat: path and querys parameters

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,5 +7,56 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "data": {
+            "message": "Hello World"
+        }
+    }
 
+
+@app.get("/blog")
+async def blog_list(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {
+            "data": {
+                f"{limit} published blog list from db"
+            }
+        }
+    else:
+        return {
+            "data": {
+                f"{limit} blog list from db"
+            }
+        }
+
+
+@app.get('/blog/unpublished')
+async def unpublished():
+    return {
+        "data": {
+            "message": "Unpublished Blog"
+        }
+    }
+
+
+@app.get('/blog/{id}')
+async def blog(id: int):
+    return {
+        "data": {
+            id: "Blog Post"
+        }
+    }
+
+
+@app.get('/blog/{id}/comments')
+async def comments(id: int, limit: int = 10):
+    return {
+        "data": {
+            id: {
+                f"comments {limit}": [
+                    "Comment 1",
+                    "Comment 2"
+                ]
+            }
+        }
+    }
