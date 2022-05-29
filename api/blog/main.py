ec523b72a1b9bc0af0fde3ed133f55de54a06f63
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-29 10:47:18 -0300

    feat: trating exceptions and correct status code

--- api/blog/main.py
+++ api/blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, HTTPException, status
 from sqlalchemy.orm import Session
 
 from . import models
@@ -10,8 +10,8 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-@app.post("/blog")
-async def create_blog(request: Blog, db: Session = Depends(get_db)):
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+async def create(request: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(**request.dict())
     db.add(new_blog)
     db.commit()
@@ -26,7 +26,9 @@ async def all_blogs(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{id}")
-async def get_single_blog(id: int, db: Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+async def get_single_blog(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} not found")
     return blog
