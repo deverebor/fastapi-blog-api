01148cfbb10e9ab4cbf05292226e52c075e33fbf
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-29 12:24:35 -0300

    fix(blog endpoint): remove /blog prefix inside in all endpoints

--- api/routers/blog/blog.py
+++ api/routers/blog/blog.py
@@ -13,6 +13,22 @@ router = APIRouter()
 models.Base.metadata.create_all(bind=engine)
 
 
+@router.get("", status_code=status.HTTP_200_OK, response_model=List[ShowBlogSchema])
+async def all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(BlogModel).all()
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog not found")
+    return blogs
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=ShowBlogSchema)
+async def get_single_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} not found")
+    return blog
+
+
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create(request: BlogSchema, db: Session = Depends(get_db)):
     new_blog = BlogModel(**request.dict())
@@ -23,17 +39,7 @@ async def create(request: BlogSchema, db: Session = Depends(get_db)):
     return new_blog
 
 
-@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def destroy(id: int, db: Session = Depends(get_db)):
-    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"success: {False}")
-    db.delete(blog)
-    db.commit()
-    return Response(status_code=status.HTTP_200_OK, content=f"success: {True}")
-
-
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 async def update(id: int, request: BlogSchema, db: Session = Depends(get_db)):
     blog = db.query(BlogModel).filter(BlogModel.id == id).first()
     if not blog:
@@ -44,17 +50,11 @@ async def update(id: int, request: BlogSchema, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_200_OK, content=f"success: {True}")
 
 
-@router.get("", status_code=status.HTTP_200_OK, response_model=List[ShowBlogSchema])
-async def all_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(BlogModel).all()
-    if not blogs:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog not found")
-    return blogs
-
-
-@router.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=ShowBlogSchema)
-async def get_single_blog(id: int, db: Session = Depends(get_db)):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def destroy(id: int, db: Session = Depends(get_db)):
     blog = db.query(BlogModel).filter(BlogModel.id == id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} not found")
-    return blog
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"success: {False}")
+    db.delete(blog)
+    db.commit()
+    return Response(status_code=status.HTTP_200_OK, content=f"success: {True}")
