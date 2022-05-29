51b75a45f0c7e71d489a3fb6ffebc1b21b5a5947
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-29 10:59:12 -0300

    feat: delete one blog

--- api/blog/main.py
+++ api/blog/main.py
@@ -20,6 +20,17 @@ async def create(request: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Is not possible destroy the id: {id} not "
+                                                                          f"found")
+    db.delete(blog)
+    db.commit()
+    return Response(status_code=status.HTTP_200_OK)
+
+
 @app.get("/blog")
 async def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
