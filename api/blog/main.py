863b0acd400c7d642c0e48c6f9034e65db63de16
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-29 10:26:10 -0300

    feat: store blog to database

--- api/blog/main.py
+++ api/blog/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from . import models, database
+from . import models
+from .database import engine, get_db
 from .schemas import Blog
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=database.engine)
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.post("/blog")
-async def create_blog(request: Blog):
-    return request
+async def create_blog(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(**request.dict())
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return new_blog
