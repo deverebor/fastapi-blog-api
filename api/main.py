255fc383f9cd83dd42c8eb2d3eb3c7dad296cea5
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-30 13:36:17 -0300

    release: version 1.0

--- api/main.py
+++ api/main.py
@@ -1,16 +1,39 @@
 from fastapi import FastAPI
 
-from api.docs.doc import ApiInfo
-
 from api.routers.blog import blog
 from api.routers.users import users
 from api.routers.auth import auth
 
+title = "Blog API"
+description = """
+    This is a simple API for a blog. It is built with FastAPI and uses SQLAlchemy for the database with SQLite.
+
+    # Basic usage
+
+    ## Blog endpoint
+
+    Get: all the posts by using the `/blog` endpoint.
+    Get: a specific post by using the `/blog/<int:id>` endpoint.
+    Post: a new blog post by using the `/blog` endpoint.
+    Put: update a specific post by using the `/blog/<int:id>` endpoint.
+    Delete: delete a specific post by using the `/blog/<int:id>` endpoint.
+
+    ## User endpoint
+
+    Get: all the users by using the `/users` endpoint.
+    Get: specific user by using the `/users/<int:id>` endpoint.
+    """
+version = "1.0"
+contact = {
+    "name": "Lucas Souza (@deverebor)",
+    "url": "https://www.oerebor.dev",
+}
+
 app = FastAPI(
-    title=ApiInfo.title,
-    description=ApiInfo.description,
-    version=ApiInfo.version,
-    contact=ApiInfo.contact
+    title=title,
+    description=description,
+    version=version,
+    contact=contact
 )
 
 app.include_router(blog.router)
